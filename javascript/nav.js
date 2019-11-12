2bdd6abd66eb7c77bddbe8ed84e0240af343923b
karapeoples <52808542+karapeoples@users.noreply.github.com>
2019-11-11 20:05:41 -0600

    Skills Done

--- javascript/nav.js
+++ javascript/nav.js
@@ -6,19 +6,31 @@ let menuItems = [
     'Contact',
   ];
 
+  
 
-  function createMenu(array) {
+
+  function createMenu() {
     const menu = document.createElement('div')
-    menu.classList.add('menu');
-  
     const menuList = document.createElement('ul')
+    const link1 = document.createElement('a');
+    const link2 = document.createElement('a');
+    const link3 = document.createElement('a');
+    const link4 = document.createElement('a');
+    const link5 = document.createElement('a');
+
+    menu.classList.add('menu');
     menu.append(menuList);
-  
-    array.forEach(info => {
-      const items = document.createElement('li');
-      items.textContent = info;
-      menuList.append(items);
-    });
+    menuList.append(link1, link2, link3, link4, link5);
+   
+      link1.href = `index.html`;
+      link1.textContent = `Home`;
+      link2.href =`github.html`;
+      link2.textContent = `GitHub`;
+      link3.href =`skills.html`;
+      link3.textContent = `Skills`;
+     
+      
+    
     
     const hamburger = document.querySelector('.menu-button');
     hamburger.addEventListener('click', () => {
